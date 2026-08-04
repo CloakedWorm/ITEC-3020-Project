05e501edb43f6ce34c080f9a0e0949fcb51ec7d8
Divyansh Bhatti <bhatti.divyansh22@gmail.com>
2026-08-04 07:50:26 -0400

    + javascript for filtering the blogpost, ~ only filters the main content and title of the post

---
+++ query.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filterInput = document.getElementById("filter-input");
+    const noResults = document.getElementById("no-results");
+
+    filterInput.addEventListener("input", function () {
+        const cards = document.querySelectorAll(".blog-post");
+
+        const query = this.value.toLowerCase().trim();
+
+        let visibleCount = 0;
+        cards.forEach(function (card) {
+            const title =
+                card.querySelector(".card-title")
+                    .textContent.toLowerCase();
+            const description =
+                card.querySelector(".card-description")
+                    .textContent.toLowerCase();
+            if (
+                title.includes(query) ||
+                description.includes(query)
+            ) {
+                card.style.display = "";
+                visibleCount++;
+            }
+            else {
+                card.style.display = "none";
+            }
+        });
+
+        if (visibleCount === 0) {
+            noResults.style.display = "block";
+        }
+        else {
+            noResults.style.display = "none";
+        }
+    });
+});
